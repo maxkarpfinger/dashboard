0f1afc1fb697d97f725c6ea66cb0818709846ec7
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2022-05-27 11:02:41 +0200

    FIXED size of icons in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,16 +15,27 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
     <TooltipComponent content={title} position="BottomCenter">
         <button type="button" onClick={customFunc} style ={{color}} className="relative text-xl rounded-full p-3 hover:bg-light-gray">
             <span style={{background:dotColor}}
-                  className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
+                  className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"/>
                 {icon}
-            </span>
         </button>
     </TooltipComponent>
 )
 
 const Navbar = () => {
-    const {activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick} = useStateContext();
-
+    const {activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize} = useStateContext();
+    useEffect(()=>{
+        const handleResize = () => setScreenSize(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    useEffect(()=>{
+        if(screenSize <= 900){
+            setActiveMenu(false);
+        }else{
+            setActiveMenu(true);
+        }
+    }, [screenSize]);
     return (
         <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
             <NavButton
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,9 +12,11 @@ const initialState = {
 export const ContextProvider = ({children}) =>{
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
     const handleClick = (clicked) => {
         setIsClicked({...initialState, [clicked]:true});
     }
+
     return (
         <StateContext.Provider
         value={{
@@ -22,7 +24,9 @@ export const ContextProvider = ({children}) =>{
             setActiveMenu,
             isClicked,
             setIsClicked,
-            handleClick
+            handleClick,
+            screenSize,
+            setScreenSize
         }}>
             {children}
         </StateContext.Provider>
